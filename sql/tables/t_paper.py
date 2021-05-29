194140fc26b6aaebcbd9fb82ab09aaba9ecfb856
FantWings <zhijun@furry.top>
2021-05-29 20:43:03 +0400

    后端表结构更新

--- sql/tables/t_paper.py
+++ sql/tables/t_paper.py
@@ -6,7 +6,7 @@ class Paper(db.Model):
     __tablename__ = "papers"
     id = db.Column(db.String(64), primary_key=True, comment='试卷号')
     paper_name = db.Column(db.String(64), nullable=True, comment='试卷名称')
-    owner = db.Column(db.String(64), nullable=True, comment='试卷名称')
+    owner = db.Column(db.String(64), nullable=False, comment='试卷名称')
     submit_ip = db.Column(db.String(15), default='0.0.0.0', comment='提交者IP地址')
     submit_time = db.Column(db.DateTime,
                             server_default=func.now(),
